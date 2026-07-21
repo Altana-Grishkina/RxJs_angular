ec0a1198f7f7d145e1f51463a7a1c163c27debc0
Altana <altn.grishkina@capgemini.com>
2026-07-22 00:00:10 +0200

    Design pattern - Stateless Observable-based serviscs

---
+++ src/app/services/courses.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Course } from "../model/course";
+import { Observable } from "rxjs";
+import { map } from "rxjs/internal/operators/map";
+
+@Injectable({
+     providedIn: 'root'
+})
+export class CoursesService{
+
+    constructor(private http: HttpClient){
+
+    }
+
+    loadAllCourses(): Observable<Course[]>{
+        return this.http.get<Course[]>("/api/courses")
+        .pipe(
+            map(res => res["payload"])
+        );
+    }
+}
