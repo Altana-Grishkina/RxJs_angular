ce66225758daa126503aeb909dd9f5ec5a118c0f
Altana <altn.grishkina@capgemini.com>
2026-07-25 17:04:49 +0200

    Avoiding angular duplicate HTTP requests with th RxJs shareReplay operator

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -29,7 +29,7 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     const courses$ = this.coursesService.loadAllCourses()
       .pipe(
-        map(courses => courses.sort(sortCoursesBySeqNo));
+        map(courses => courses.sort(sortCoursesBySeqNo))
       );
     this.beginnerCourses$ = courses$.pipe(
       map(courses => courses.filter(course => course.category === "BEGINNER"))
--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from "@angular/core";
 import { Course } from "../model/course";
 import { Observable } from "rxjs";
 import { map } from "rxjs/internal/operators/map";
+import { shareReplay } from "rxjs/operators";
 
 @Injectable({
      providedIn: 'root'
@@ -16,7 +17,8 @@ export class CoursesService{
     loadAllCourses(): Observable<Course[]>{
         return this.http.get<Course[]>("/api/courses")
         .pipe(
-            map(res => res["payload"])
+            map(res => res["payload"]),
+            shareReplay()
         );
     }
 }
