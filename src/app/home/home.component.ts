d870ea62de18e56767ca70663ca76bd562c81962
Altana <altn.grishkina@capgemini.com>
2026-09-15 11:33:39 +0200

    22. Initial implementation of a store service

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,7 @@ import {CourseDialogComponent} from '../course-dialog/course-dialog.component';
 import { CoursesService } from '../services/courses.service';
 import { LoadingService } from '../loading/loading.service';
 import { MessagesService } from '../messages/messages.service';
+import { CoursesStore } from '../services/courses.store';
 
 
 @Component({
@@ -24,10 +25,7 @@ export class HomeComponent implements OnInit {
   advancedCourses$: Observable<Course[]>;
 
 
-  constructor(private coursesService: CoursesService, 
-              private dialog: MatDialog,
-            private loadingService: LoadingService,
-          private messagesService: MessagesService) {
+  constructor(private courseStore: CoursesStore) {
 
   }
 
@@ -37,25 +35,8 @@ export class HomeComponent implements OnInit {
 
   reloadCourses() {
 
-    const courses$ = this.coursesService.loadAllCourses()
-      .pipe(
-        map(courses => courses.sort(sortCoursesBySeqNo)),
-        catchError(err => {
-          const message = 'Could not load courses';
-          this.messagesService.showErrors(message);
-          console.log(message, err);
-          return throwError(err);
-        })
-      );
-
-    const loadedCourses$ = this.loadingService.showLoaderUntilCompleted<Course[]>(courses$);
-
-    this.beginnerCourses$ = loadedCourses$.pipe(
-      map(courses => courses.filter(course => course.category === "BEGINNER"))
-    );
-    this.advancedCourses$ = loadedCourses$.pipe(
-      map(courses => courses.filter(course => course.category === "ADVANCED"))
-    );
+    this.beginnerCourses$ = this.courseStore.filterByCategory("BEGINNER");
+    this.advancedCourses$ = this.courseStore.filterByCategory("ADVANCED");
   }
   editCourse(course: Course) {
 
---
+++ src/app/services/courses.store.ts
@@ -0,0 +1,20 @@
+import {Injectable} from '@angular/core';
+import { Course, sortCoursesBySeqNo } from '../model/course';
+import { Observable } from 'rxjs/internal/Observable';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class CoursesStore {
+    courses$: Observable<Course[]>;
+
+    filterByCategory(category: string): Observable<Course[]> {
+        return this.courses$.pipe(
+            map(courses =>
+                courses.filter(course => course.category === category)
+                .sort(sortCoursesBySeqNo);
+            )
+        );
+    }
+}
